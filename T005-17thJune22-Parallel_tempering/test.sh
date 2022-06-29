a1ca5743bb3665bc4f0d990f39654f77bbb32f3f
godofwarnings <102691547+godofwarnings@users.noreply.github.com>
2022-06-29 16:28:08 +0530

    Add files via upload

--- T005-17thJune22-Parallel_tempering/test.sh
+++ T005-17thJune22-Parallel_tempering/test.sh
@@ -4,11 +4,12 @@ mkdir ptcIs
 
 cd ptcIs
 
-wget https://github.com/godofwarnings/CiS1/blob/main/files.zip?raw=true
-wget https://github.com/godofwarnings/CiS1/blob/main/script.sh?raw=true
+wget https://raw.githubusercontent.com/godofwarnings/CiS1/main/T005-17thJune22-Parallel_tempering/script.sh
+wget https://github.com/godofwarnings/CiS1/blob/main/T005-17thJune22-Parallel_tempering/files.zip?raw=true
 
 mv files.zip?raw=true files.zip
 mv script.sh?raw=true script.sh
 
-cd ptcIs
 chmod +x script.sh
+
+clear
