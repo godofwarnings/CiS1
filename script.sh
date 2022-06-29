c393450bccc97cdcabc03c8a53835fe8db7606ba
godofwarnings <102691547+godofwarnings@users.noreply.github.com>
2022-06-29 12:48:42 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -181,3 +181,28 @@ done
 # 3. For Loop over replicas
 # 4. For Loop over replicas for production. output to .log file
 # 5. Make a function to update energies for all energies
+mkdir data
+
+for ((i = 1; i <= $repls; i++)); do
+    touch ./data/data_$i
+    echo "[" >>./data/data_$i
+done
+
+find_energy_2() {
+    count=$(grep -o ENERGY ./main/output_$1/prod_$2/energy_out.log | wc -l)
+    for ((pp = 11; pp <= $count; pp++)); do
+        line=$(awk "/ENERGY/{++n; if (n==$pp) { print NR; exit}}" ./main/output_$1/prod_$2/energy_out.log)
+        temp=$(awk "NR==$line{ print; exit }" ./main/output_$1/prod_$2/energy_out.log)
+        value=(${temp})
+        energy=${value[11]}
+        echo $energy"," >>./data/data_$1
+    done
+
+}
+
+for ((i = 1; i <= $repls; i++)); do
+    for ((j = 0; j <= $prodss; j++)); do
+        find_energy_2 $i $j
+    done
+    echo "]" >>./data/data_$i
+done
