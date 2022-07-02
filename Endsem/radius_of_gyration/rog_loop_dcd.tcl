26a07ace66e92d92d841c8454d9043217eb2427b
godofwarnings <102691547+godofwarnings@users.noreply.github.com>
2022-07-02 15:50:14 +0530

    Add files via upload

--- Endsem/radius_of_gyration/rog_loop_dcd.tcl
+++ Endsem/radius_of_gyration/rog_loop_dcd.tcl
@@ -1,6 +1,3 @@
-mol new /home/devanandt/Documents/RAS/1C1Y/DATA/1C1Y/ANALYSIS/fitted_DCD/APO/1C1Y_APO_RAS_200ns_fitted.psf
-mol addfile /home/devanandt/Documents/RAS/1C1Y/DATA/1C1Y/ANALYSIS/fitted_DCD/APO/1C1Y_APO_RAS_200ns_fitted.dcd first 0 last -1 step 100 waitfor all
-
 # load necessary tcl functions  (Ref : http://www.ks.uiuc.edu/Research/vmd/vmd-1.7.1/ug/node182.html )
 source gyr_radius.tcl
 source center_of_mass.tcl
